ba90c70f852b1e1b74374d7b4156fdc8120993ef
JamesL <typus24@hotmail.com>
2026-01-28 12:11:33 +0000

    MD-957 Range replacement transformation

--- src/5-surface-components/ContentElementHandler.ts
+++ src/5-surface-components/ContentElementHandler.ts
@@ -1,4 +1,5 @@
-import type { WebComponentElement, WebContentSurfaceBehavior, WebElement } from '../types'
+import type { WebContentSurfaceBehavior } from '../types'
+import { renderElement } from './elements'
 
 const transformPositions = ['before', 'prepend', 'append', 'after'] as const
 const reverseTransformPositions = ['after', 'prepend']
@@ -20,7 +21,7 @@ export class ContentElementHandler implements HTMLRewriterElementContentHandlers
             if (this.content[key]?.length) {
                 ;(reverseTransformPositions.includes(key) ? [...this.content[key]].reverse() : this.content[key]).forEach(
                     (transformation) => {
-                        element[key](...this.renderElement(transformation))
+                        element[key](...renderElement(transformation))
                     },
                 )
                 retainElement = true
@@ -34,41 +35,5 @@ export class ContentElementHandler implements HTMLRewriterElementContentHandlers
                 element.remove()
             }
         }
-    }
-
-    private renderElement(element: WebElement): [string, ContentOptions] {
-        try {
-            const mapped = {
-                ...element,
-                type: element.type?.toLowerCase(),
-            } as WebElement
-
-            if (mapped.type === 'html') {
-                return [mapped.content, { html: true }]
-            }
-
-            if (mapped.type === 'text') {
-                return [mapped.content, { html: false }]
-            }
-
-            if (mapped.type === 'element') {
-                return [this.renderComponentElement(mapped), { html: true }]
-            }
-        } catch (error) {
-            console.error('Error rendering element:', error)
-            return ['', { html: false }]
-        }
-
-        console.warn(`Unsupported element type: ${element.type}`)
-        return ['', { html: false }]
-    }
-
-    private renderComponentElement(component: WebComponentElement): string {
-        const [schemaSource, versionedSchemaId] = component.schema.split(':')
-        const [schemaId, schemaVersion] = versionedSchemaId?.split('@') ?? []
-        const webComponentTag = `${schemaSource}-${schemaId}`
-        const escapedPropsAttribute = JSON.stringify(component.props).replace(/"/g, '&quot;')
-
-        return `<${webComponentTag} version="${schemaVersion ?? ''}" props="${escapedPropsAttribute}"></${webComponentTag}>`
     }
 }
